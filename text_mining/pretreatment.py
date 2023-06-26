75329648748e4e1a5378336c45ae825730a55ecd
lky <1464329490@qq.com>
2023-06-26 13:26:04 +0800

    预处理

---
+++ text_mining/pretreatment.py
@@ -0,0 +1,20 @@
+import re
+import jieba
+class pretreatment:
+    def __init__(self):
+        pass
+
+    def non_text(self):
+        pass
+
+    def split(self):
+        pass
+
+    def word_tag(self):
+        pass
+
+    def stop_word_filter(self):
+        pass
+
+    def vsm(self):
+        pass
